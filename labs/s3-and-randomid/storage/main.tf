8874ef875fd206ec7ac9a578c0e217039641859f
Sanjar <sanjar.devops@gmail.com>
2019-07-15 10:21:58 -0500

    s3 and random-id altered

--- labs/s3-and-randomid/storage/main.tf
+++ labs/s3-and-randomid/storage/main.tf
@@ -4,13 +4,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a random id 
-
+# Create a random id
+resource "random_id" "tf_bucket_id" {
+  byte_length = 2
+}
 
 # Create the bucket
-
 resource "aws_s3_bucket" "tf_code" {
-  bucket        = "${var.project_name}"
+  bucket        = "${var.project_name}-${random_id.tf_bucket_id.dec}"
   acl           = "private"
   force_destroy = true
 
